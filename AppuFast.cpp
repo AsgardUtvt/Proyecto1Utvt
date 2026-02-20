69b4124f2c019186ca67961b84c66e6c32c94f26
Asgard <al222510064@gmail.com>
2026-02-19 21:53:18 -0600

    add funcion Menu y LogoAscii

--- AppuFast.cpp
+++ AppuFast.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 using namespace std;
+void LogoAscii();
+void MenuCosas();
+
 int main()
 {
-	cout << "Hola mundo";
-	cout << "Hola como estan" << endl;
+	LogoAscii();
+	MenuCosas();
 	return 0;
+}
+
+
+void LogoAscii()
+{
+
+	cout << "   ##     #####    #####    ##  ##   ######     ##      ####    ###### " << endl;
+	cout << "  ####    ##  ##   ##  ##   ##  ##   ##        ####    ##         ## " << endl;
+	cout << " ##  ##   ##  ##   ##  ##   ##  ##   #####    ##  ##    ####      ## " << endl;
+	cout << " ##  ##   #####    #####    ##  ##   ##       ##  ##       ##     ## " << endl;
+	cout << " ######   ##       ##       ##  ##   ##       ######       ##     ## " << endl;
+	cout << " ##  ##   ##       ##       ######   ##       ##  ##    ####      ## " << endl;
+
+
+}
+
+void MenuCosas()
+{
+
+	cout << "Bienvenidos a AppuFast, la aplicación de envío de comida." << endl;
+	cout << "1. Productos" << endl;
+	cout << "2. Carrito" << endl;
+	cout << "3. Historial de compras" << endl;
+	cout << "4. Pedidos" << endl;
+	cout << "5. Configuración" << endl;
+	cout << "Selecciona una opción: " << endl;
+
 }
