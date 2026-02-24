50f1f20c2e9ee07b5e83cd19ab3b8ea3f4c32779
luis <al222510464@gmail.com>
2026-02-24 14:47:52 -0600

    Cambios Luis

--- AppuFast.cpp
+++ AppuFast.cpp
@@ -43,10 +43,42 @@ int main()
 				break;
 			case 2:
 				//Carrito
+				cout << "=== CARRITO ===\n";
+				cout << "porductos en tu carrito.\n";
+				cout << "1. Hamburguesa - $120\n";
+                cout << "2. Papas - $80\n";
+                cout << "3. Refresco - $50\n";
+                cout << "Total actual: $" << total << endl;
+
+                cout << "\n1. Vaciar carrito\n";
+                cout << "2. Volver al menú\n";
+                cout << "Selecciona una opción: ";
+                cin >> opcionCarrito;
+
+                if(opcionCarrito == 1)
+                {
+                    cout << "Carrito vaciado correctamente.\n";
+                    total = 0;
+                }
 
 				break;
 			case 3:
 				//Historial de compras
+				cout << "=== HISTORILA DE COMPRAS ===\n";
+				cout << "Compra #1\n";
+                cout << "Fecha: 20/02/2026\n";
+                cout << "Total: $320\n";
+                cout << "Estado: Entregado\n\n";
+
+                cout << "Compra #2\n";
+                cout << "Fecha: 18/02/2026\n";
+                cout << "Total: $150\n";
+                cout << "Estado: Entregado\n\n";
+
+                cout << "Compra #3\n";
+                cout << "Fecha: 15/02/2026\n";
+                cout << "Total: $280\n";
+                cout << "Estado: Cancelado\n";
 
 				break;
 			case 4:
@@ -55,10 +87,14 @@ int main()
 				break;
 			case 5:
 				//Configuracion
+				cout << "=== CONFIGURACIÓN ===\n";
+                cout << "1. Cambiar nombre\n";
+                cout << "2. Cambiar contraseña\n";
 
 				break;
 			case 6:
 				//Salir
+				
 				cout << "Gracias por usar AppuFast\n";
 				break;
 			default:
