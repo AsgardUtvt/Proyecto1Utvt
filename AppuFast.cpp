8eeec5a07516503ec1720fba1acedbcf35831c2f
Asgard <al222510064@gmail.com>
2026-02-28 00:51:05 -0600

    Se termino el incio de sesion.

--- AppuFast.cpp
+++ AppuFast.cpp
@@ -1,22 +1,63 @@
 #include <iostream>
-#include <cstdlib>
-
-using namespace std;
-
-/*
-//Inicializacion de funciones.
-void LogoAscii();
+using namespace std; /* //Inicializacion de funciones.  void LogoAscii();
 void MenuCosas();
 void LimpiarPantalla();
 void EsperarTecla();
 */
 int main()
 {
-	bool salir = false;
-	int opcMen = 0; 
+	bool regi=false;
+	int opcMen = 0;
+	string nombre,contra,contraUs,nombreUs;
+	int opcIncioSes = 0;
+	while(regi != true)
+	{
+		cout << "1. Inicio sesion" << endl;
+		cout << "2. Registrarse" << endl;
+		cin >> opcIncioSes;
+	switch(opcIncioSes)
+	{
+		case 1:
+			if(nombre == "" && contra == "")
+			{
+				cout << "Por favor genera tu cuenta\n";
+			}else
+			{
 
+				cout << "Ingresa tu nombre: \n";
+				cin >> nombreUs;
+				cout << "Ingresa la contraseña\n";
+				cin >> contraUs;
+				if(nombreUs == nombre && contraUs == contra)
+				{
+					cout << "Has iniciado sesion\n";
+					regi = true;
+				}else
+				{
+					cout << "Contraseña o usuario incorrecto\n";
+				}
+								
+				
+				
+			}
+
+			break;
+		case 2:
+			cout << "Ingresa tu nombre: \n";
+			cin >> nombre;
+			cout << "Ingresa la contraseña\n";
+			cin >> contra;
+			break;
+	}
+	}
+
+
+	opcMen = 0; 
+	int total, opcionCarrito;
 	while(opcMen != 6)
 	{
+	
+
 		opcMen = 0; 
 	
 		cout << "   ##     #####    #####    ##  ##   ######     ##      ####    ###### " << endl;
@@ -46,14 +87,14 @@ int main()
 				cout << "=== CARRITO ===\n";
 				cout << "porductos en tu carrito.\n";
 				cout << "1. Hamburguesa - $120\n";
-                cout << "2. Papas - $80\n";
-                cout << "3. Refresco - $50\n";
-                cout << "Total actual: $" << total << endl;
+				cout << "2. Papas - $80\n";
+				cout << "3. Refresco - $50\n";
+				cout << "Total actual: $" << total << endl;
 
-                cout << "\n1. Vaciar carrito\n";
-                cout << "2. Volver al menú\n";
-                cout << "Selecciona una opción: ";
-                cin >> opcionCarrito;
+				cout << "\n1. Vaciar carrito\n";
+				cout << "2. Volver al menú\n";
+				cout << "Selecciona una opción: ";
+				cin >> opcionCarrito;
 
                 if(opcionCarrito == 1)
                 {
@@ -66,19 +107,19 @@ int main()
 				//Historial de compras
 				cout << "=== HISTORILA DE COMPRAS ===\n";
 				cout << "Compra #1\n";
-                cout << "Fecha: 20/02/2026\n";
-                cout << "Total: $320\n";
-                cout << "Estado: Entregado\n\n";
+				cout << "Fecha: 20/02/2026\n";
+				cout << "Total: $320\n";
+				cout << "Estado: Entregado\n\n";
 
-                cout << "Compra #2\n";
-                cout << "Fecha: 18/02/2026\n";
-                cout << "Total: $150\n";
-                cout << "Estado: Entregado\n\n";
+				cout << "Compra #2\n";
+				cout << "Fecha: 18/02/2026\n";
+				cout << "Total: $150\n";
+				cout << "Estado: Entregado\n\n";
 
-                cout << "Compra #3\n";
-                cout << "Fecha: 15/02/2026\n";
-                cout << "Total: $280\n";
-                cout << "Estado: Cancelado\n";
+				cout << "Compra #3\n";
+				cout << "Fecha: 15/02/2026\n";
+				cout << "Total: $280\n";
+				cout << "Estado: Cancelado\n";
 
 				break;
 			case 4:
@@ -88,8 +129,8 @@ int main()
 			case 5:
 				//Configuracion
 				cout << "=== CONFIGURACIÓN ===\n";
-                cout << "1. Cambiar nombre\n";
-                cout << "2. Cambiar contraseña\n";
+				cout << "1. Cambiar nombre\n";
+				cout << "2. Cambiar contraseña\n";
 
 				break;
 			case 6:
@@ -121,29 +162,55 @@ void LogoAscii()
 	cout << " ######   ##       ##       ##  ##   ##       ######       ##     ## " << endl;
 	cout << " ##  ##   ##       ##       ######   ##       ##  ##    ####      ## " << endl;
 
+=======
+void MenuPrincipal();
+void LogotipoAscii();
+int main()
+{
+	cout << "Hola mundo";
+	cout << "Hola como estan" << endl;
+	return 0;
+>>>>>>> Stashed changes
 
 }
 
-void MenuCosas()
+
+
+
+void MenuPrincipal()
 {
 
 	cout << "Bienvenidos a AppuFast, la aplicación de envío de comida." << endl;
 	cout << "1. Productos" << endl;
-	cout << "2. Carrito" << endl;
-	cout << "3. Historial de compras" << endl;
-	cout << "4. Pedidos" << endl;
-	cout << "5. Configuración" << endl;
-	cout << "6. Salir" << endl;
-	cout << "Selecciona una opción: " << endl;
+	cout << "2. Carrito " << endl;
+	cout << " " << endl;
+	
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+
+
+
+
+
 
 
 }
 
-
-void LimpiarPantalla()
+void LogotipoAscii()
 {
-	system("clear||cls");
+
+
+	cout<<"   ##     #####    #####    ##  ##   ######     ##      ####    ######"<<endl;
+	cout<<"	 ####    ##  ##   ##  ##   ##  ##   ##        ####    ##         ## "<< endl;
+	cout<<" ##  ##   ##  ##   ##  ##   ##  ##   #####    ##  ##    ####      ## "<< endl;
+	cout<<" ##  ##   #####    #####    ##  ##   ##       ##  ##       ##     ## "<< endl;
+	cout<<" ######   ##       ##       ##  ##   ##       ######       ##     ## "<< endl;
+	cout<<" ##  ##   ##       ##       ######   ##       ##  ##    ####      ## " << endl;
+
 }
+<<<<<<< Updated upstream
 
 void EsperarTecla()
 {
